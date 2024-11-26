2a31c2edee63589d2029293961f221f2f0a9954a
ALVEENA <alvna677@gmail.com>
2024-11-26 16:31:31 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import TodoList from "@/components/ui/todo-list"; // Capitalize the imported component name
+
+const TodoListPage = () => {
+  return (
+    <div>
+      <TodoList /> {/* Capitalize the component name when used in JSX */}
+    </div>
+  );
+};
+
+export default TodoListPage;
+
